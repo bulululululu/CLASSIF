8b98ea9ada7dc95476ad38ca1dce605adda18242
bulululululu <117985482+bulululululu@users.noreply.github.com>
2025-04-25 14:43:56 +0200

    Update GIGA_TEST_COMM__I2C.ino

--- GIGA_TEST_COMM__I2C.ino
+++ GIGA_TEST_COMM__I2C.ino
@@ -1,7 +1,3 @@
-//SDA Giga (D20) -> SDA Metro M0
-//SCL Giga (D21) -> SCL Metro M0
-//GND -> GND
-
 #include <Wire.h>
 
 const byte slaveAddress = 0x08;
@@ -9,20 +5,20 @@ int valueToSend = 0;
 
 // SETUP - RUNS ONCE AT PROGRAM START --------------------------------------
 void setup() {
-  Wire.begin(); // Initialise en maître
+  Wire1.begin(); // Initialise en maître
   Serial.begin(9600);
 }
 
 // LOOP - RUNS REPEATEDLY AFTER SETUP --------------------------------------
 void loop() {
-  valueToSend = (valueToSend + 1) % 100; // Valeur de test entre 0 et 99
+  valueToSend = (valueToSend + 1) % 10000; // Valeur de test entre 0 et 99
 
-  Wire.beginTransmission(slaveAddress);
-  Wire.write(valueToSend); // Envoie d'un octet
-  Wire.endTransmission();
+  Wire1.beginTransmission(slaveAddress);
+  Wire1.write(valueToSend); // Envoie d'un octet
+  Wire1.endTransmission();
 
   Serial.print("Envoyé au slave Metro : ");
   Serial.println(valueToSend);
 
-  delay(1000); 
+  delay(500); 
 }
